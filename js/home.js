a6fc782bae8e328885436c436021536662bd4bb3
Elias Ablan <eliasablan93@gmail.com>
2023-11-07 15:52:01 -0400

    typed effect on homepage

--- js/home.js
+++ js/home.js
@@ -48,16 +48,6 @@ particlesJS("particles-js", {
   retina_detect: true
 });
 
-// const setHeight = () => {
-//   const currentHeight = window.innerHeight;
-//   const elementsId = [
-//     'particles-js',
-//     'content',
-//   ]
-//   elementsId.forEach((element) => {
-//     const elementById = document.getElementById(element);
-//     elementById.style.height = `${currentHeight}px`;
-//   });
-// }
-// window.addEventListener("resize", setHeight);
-// setHeight();
+var typed = new Typed('#typed', {
+  stringsElement: '#typed-strings'
+});
