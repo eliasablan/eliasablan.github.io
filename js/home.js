8220607df8b55296f1b5ec95aa53627e479e901b
Elias Ablan <eliasablan93@gmail.com>
2023-12-12 21:13:57 -0400

    tests

--- js/home.js
+++ js/home.js
@@ -101,75 +101,75 @@ const typed = new Typed("#typed", {
 });
 
 // ********** Desplazo con flechas arriba/abajo **********
-const ordered = [
-  {
-    name: "landing",
-    ArrowDown: "bio",
-  },
-  {
-    name: "bio",
-    ArrowUp: "landing",
-    ArrowDown: "projects",
-  },
-  {
-    name: "projects",
-    ArrowUp: "bio",
-    ArrowDown: "contact",
-  },
-  {
-    name: "contact",
-    ArrowUp: "projects",
-  },
-];
+// const ordered = [
+//   {
+//     name: "landing",
+//     ArrowDown: "bio",
+//   },
+//   {
+//     name: "bio",
+//     ArrowUp: "landing",
+//     ArrowDown: "projects",
+//   },
+//   {
+//     name: "projects",
+//     ArrowUp: "bio",
+//     ArrowDown: "contact",
+//   },
+//   {
+//     name: "contact",
+//     ArrowUp: "projects",
+//   },
+// ];
 
-document.addEventListener("keydown", function (event) {
-  // Check if the key pressed is the up arrow key or the down arrow key
-  if (event.key === "ArrowUp" || event.key === "ArrowDown") {
-    event.preventDefault(); // Prevent the default behavior of arrow keys (e.g., scrolling the page)
+// document.addEventListener("keydown", function (event) {
+//   // Check if the key pressed is the up arrow key or the down arrow key
+//   if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+//     event.preventDefault(); // Prevent the default behavior of arrow keys (e.g., scrolling the page)
 
-    // Get the current section
-    let currentSection =
-      document.querySelector(".section:target") ||
-      document.querySelector(".section");
+//     // Get the current section
+//     let currentSection =
+//       document.querySelector(".section:target") ||
+//       document.querySelector(".section");
 
-    const currentOrdered = ordered.find(
-      (section) => section.name == currentSection.id
-    );
+//     const currentOrdered = ordered.find(
+//       (section) => section.name == currentSection.id
+//     );
 
-    if (!!currentOrdered[event.key]) {
-      document.location.hash = currentOrdered[event.key];
-    }
-  }
-});
+//     if (!!currentOrdered[event.key]) {
+//       document.location.hash = currentOrdered[event.key];
+//     }
+//   }
+// });
 
 // ********** Actualiza URL con scroll **********
 // Obtiene todos los elementos con ID que deseas observar
-const sectionsToObserve = document.querySelectorAll(".section");
+// const sectionsToObserve = document.querySelectorAll(".section");
 
-// Callback para la función de Intersection Observer
-function handleIntersection(entries, observer) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      // Obtén el ID del elemento que está en la parte superior de la pantalla
-      const id = entry.target.id;
+// // Callback para la función de Intersection Observer
+// function handleIntersection(entries, observer) {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       // Obtén el ID del elemento que está en la parte superior de la pantalla
+//       const id = entry.target.id;
 
-      // Actualiza la URL añadiendo el ID al final
-      history.replaceState({}, "", `#${id}`);
-    }
-  });
-}
+//       // Actualiza la URL añadiendo el ID al final
+//       history.replaceState({}, "", `#${id}`);
+//     }
+//   });
+// }
 
-// Configura el Intersection Observer
-const observer = new IntersectionObserver(handleIntersection, {
-  root: null, // Observa el viewport
-  rootMargin: "0px",
-  threshold: 0.5, // Cuando el elemento está al 50% o más visible
-});
+// // Configura el Intersection Observer
+// const observer = new IntersectionObserver(handleIntersection, {
+//   root: null, // Observa el viewport
+//   rootMargin: "0px",
+//   threshold: 0.5, // Cuando el elemento está al 50% o más visible
+// });
 
-// Observa los elementos
-sectionsToObserve.forEach((section) => {
-  observer.observe(section);
-});
+// // Observa los elementos
+// sectionsToObserve.forEach((section) => {
+//   observer.observe(section);
+// });
 
 // ********** Intento de Animaciones **********
 // const orderedSections = [
